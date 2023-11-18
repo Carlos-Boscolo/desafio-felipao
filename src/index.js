09451337e52a84a181d2cc6e0971a3811b7625ef
Carlos-Boscolo <138625712+Carlos-Boscolo@users.noreply.github.com>
2023-11-18 11:14:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ if(xp <= 1000) {
 }else if(xp >= 5001 && xp <= 7000) {
 	nivel = "Ouro"
 
-}else if(xp >= 7001 && xp <= 8000) {Q
+}else if(xp >= 7001 && xp <= 8000) {
+	nivel = "Platina"
+	
 }else if(xp >= 8001 && xp <= 9000) {
 	nivel = "Ascendente"
 
